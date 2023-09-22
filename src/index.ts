95e1ae44f7343df0513e3cf4f7a21302936f74cc
Franco Palacios <francopalacios7@yahoo.com.ar>
2023-09-22 17:41:03 -0300

    third

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { CanvasView } from './view/CanvasView'
+import { Ball } from './sprites/Ball'
+import { Brick } from './sprites/Brick'
+import { Paddle } from './sprites/Paddle'
+
+//Images
+import PADDLE_IMAGE from './images/paddle.png'
+import BALL_IMAGE from './images/ball.png'
